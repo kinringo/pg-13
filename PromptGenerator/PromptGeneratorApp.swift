465e4d03969896e3662a5f45259a268e5e14baf1
maria elena <maria.elena.francisco@gmail.com>
2026-05-02 22:04:58 -0400

    Initial Commit

---
+++ PromptGenerator/PromptGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromptGeneratorApp.swift
+//  PromptGenerator
+//
+//  Created by Maria Elena on 5/2/26.
+//
+
+import SwiftUI
+
+@main
+struct PromptGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
